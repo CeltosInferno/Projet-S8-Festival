ac16af6f7839b668036a5b0776d109e6b8aa8cfe
Millon-Devigne <48944028+Millon-Devigne@users.noreply.github.com>
2019-03-26 10:22:44 +0100

    Add files via upload

---
+++ Festival.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Festival
+{
+    private string nom;
+    private string date;
+    private string lieu;
+    private string scene;
+
+	public Festival(string nom, string date, string lieu, string scene)
+	{
+        this.nom = nom;
+        this.date = date;
+        this.lieu = lieu;
+        this.scene = scene;
+	}
+}
