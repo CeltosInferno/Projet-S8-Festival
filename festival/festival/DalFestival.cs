65fb794d8cffbab4ec3281a5e6d99370819cf7a9
Marine POIDEVIN <marine.poidevin@groupe-esigelec.org>
2019-03-27 20:37:17 +0100

    2703

--- festival/festival/DalFestival.cs
+++ festival/festival/DalFestival.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace festival
 {
-    class DalFestival
+    public class DalFestival : IDalFestival
     {
+        private BddContext bdd;
+
+        public DalFestival()
+        {
+            bdd = new BddContext();
+        }
+
+        public void CreateFestival(Festival festival)
+        {
+            bdd.Festivals.Add(festival);
+            bdd.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            bdd.Dispose();
+        }
+
+        public Festival GetFestival(string nom)
+        {
+            return bdd.Festivals.FirstOrDefault(festival => festival.Nom.Equals(nom));
+        }
+        
     }
 }
